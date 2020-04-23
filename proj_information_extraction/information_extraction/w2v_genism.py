1dd79926fdd4bfa128d7038ef3ccebd947f0fe1a
fionattu <includetujy@gmail.com>
2020-04-23 22:25:10 +0800

    explore genism w2v model by training

--- proj_information_extraction/information_extraction/w2v_genism.py
+++ proj_information_extraction/information_extraction/w2v_genism.py
@@ -1,6 +1,8 @@
 import os
 import json
 import re
+from gensim.models.word2vec import Word2Vec
+from gensim.models.callbacks import CallbackAny2Vec
 
 
 def read_file(path):
@@ -16,8 +18,23 @@ def process_text(text):
     return text
 
 
+class EpochLogger(CallbackAny2Vec):
+    """Callback to log information about training"""
+
+    def __init__(self):
+        self.epoch = 0
+
+    def on_epoch_begin(self, model):
+        print("Epoch #{} start".format(self.epoch))
+
+    def on_epoch_end(self, model):
+        self.epoch += 1
+
+
 sentences = []
 json_data_path = "/Users/fiona/Desktop/Github/IE_week2/dl_information_extract/dataset/json/"
+corpus_path = "/Users/fiona/Desktop/Github/IE_week2/dl_information_extract/dataset/w2v/corpus_1.txt"
+embedding_path = "/Users/fiona/Desktop/Github/IE_week2/dl_information_extract/dataset/w2v/sg_ns_100.txt"
 json_files = [os.path.join(json_data_path, file) for file in os.listdir(json_data_path)]
 for file in json_files:
     text = read_file(file)
@@ -26,4 +43,16 @@ for file in json_files:
         text = process_text(context['text'])
         sentences.append(' '.join(text))
 print(len(sentences), sentences[0])
-print()
+
+with open(corpus_path, 'w') as f:
+    f.write('\n'.join(sentences))
+
+with open(corpus_path, 'r') as f:
+    sentences = [line.split(' ') for line in f.read().split('\n')]
+epoch_logger = EpochLogger()
+model = Word2Vec(sentences=sentences, min_count=1, size=100, sg=1, iter=50, callbacks=[epoch_logger])
+model.wv.save_word2vec_format(embedding_path, binary=False)
+
+for word in ['病', '痛', '肚']:
+    sim_words = model.wv.most_similar(word)
+    print(word, sim_words)
