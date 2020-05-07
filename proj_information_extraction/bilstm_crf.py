59296f8f603e0792330460907a56aa403af524e6
fionattu <includetujy@gmail.com>
2020-05-07 22:24:13 +0800

    train framework; read embedding

--- proj_information_extraction/bilstm_crf.py
+++ proj_information_extraction/bilstm_crf.py
@@ -8,14 +8,16 @@ class BiLSTM_CRF(nn.module):
         super(BiLSTM_CRF, self).__init__()
         self.n_embedding = n_embedding
         self.n_hidden = n_hidden
-        self.n_voc = n_voc
+        # self.n_voc = n_voc
         self.n_tag = n_tags
-        self.embedding = nn.Embedding(n_voc, n_embedding)
+        self.embedings = nn.Embedding(n_voc, n_embedding)
         self.lstm = nn.LSTM(input_size=n_embedding, hidden_size=n_hidden, bidirectional=True)
         self.hidden2tag = nn.Linear(n_hidden * 2, n_tags, bias=True)
-        # self.hidden_state = Variable(torch.tensor())
         self.transitions = nn.Parameter(n_tags, n_tags)
 
+    def _init_embedding(self, embeddings):
+        pass
+    
     def _lstm_features(self, X): # X = [batch_size, seq_len, n_embedding]
         X = X.transpose(0, 1) # X = [seq_len, batch_size, n_embedding]
         hidden_state = Variable(torch.zeros(1 * 2, list(X.size())[1], self.n_hidden))
--- proj_information_extraction/train.py
+++ proj_information_extraction/train.py
@@ -1,17 +1,30 @@
 import pickle
+import numpy as np
+import torch
+import torch.nn as nn
+from torch import optim
+from torch.autograd import Variable
+
+from bilstm_crf import BiLSTM_CRF
 
 pkl_fname = "data/msra_ner.pkl"
+
 with open(pkl_fname, 'rb') as f:
     word2id = pickle.load(f)
     id2word = pickle.load(f)
+
     tag2id = pickle.load(f)
     id2tag = pickle.load(f)
+
     x_train = pickle.load(f)
     y_train = pickle.load(f)
+
     x_test = pickle.load(f)
     y_test = pickle.load(f)
+
     x_valid = pickle.load(f)
     y_valid = pickle.load(f)
+
 print("word2id size: {}, id2word size: {}".format(len(word2id), len(id2word)))
 print("word 'pad' id: {}, 'unk' id: {}".format(word2id['pad'], word2id['unk']))
 assert len(word2id) == len(id2word)
@@ -20,7 +33,61 @@ assert len(x_train) == len(y_train)
 assert len(x_test) == len(y_test)
 assert len(x_valid) == len(y_valid)
 
+word2embeds = {}
+with open('dataset/vec.txt', 'r') as f:
+    for line in f:
+        char = line[0]
+        embedding = line.replace(line[0], '').strip().split()
+        word2embeds[char] = embedding
+f.close()
+
+embedding_dim = 100
+embeddings = np.zeros((len(word2id), embedding_dim))
+for word, id in word2id.items():
+    if word in word2embeds:
+        embeddings[id] = word2embeds[word]
+    else:
+        print('no embedding: {}'.format(word))
+
+
 START_TAG = '<START>'
 END_TAG = '<END>'
 tag2id[START_TAG] = len(tag2id)
 tag2id[END_TAG] = len(tag2id)
+batch_size = 10  # depends on memory
+n_epoches = 100
+dtype = torch.FloatTensor
+
+
+def random_batch(x_train, y_train, batch_size):
+    batch_inputs = []
+    batch_output = []
+    random_indices = np.random.choice(range(len(x_train)), batch_size, replace=False)
+
+    for i in random_indices:
+        batch_inputs.append(x_train[i])
+        batch_output.append(y_train[i])
+
+    return batch_inputs, batch_output
+
+
+model = BiLSTM_CRF()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+
+for epoch in range(n_epoches):
+    optimizer.zero_grad()
+
+    batch_inputs, batch_outputs = random_batch(x_train, y_train, batch_size)
+    batch_inputs = Variable(torch.LongTensor(batch_inputs))
+    batch_output = Variable(torch.Tensor(batch_outputs))
+
+    outputs = model(batch_inputs)
+    loss = criterion(outputs, batch_output)
+
+    loss.backward()
+    optimizer.step()
+
+    if (epoch + 1) % 10 == 0:
+        print('Epoch: {:04d}'.format(epoch))
